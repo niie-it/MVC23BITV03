587d019dbbd1e10cf153bd36d3a1b44a5aa24b57
HIENLTH <hyhien@gmail.com>
2026-07-29 09:00:06 +0700

    Demo Authentication

---
+++ DemoAuthen/DemoAuthen/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoAuthen.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ DemoAuthen/DemoAuthen/Data/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoAuthen.Data
+{
+    [Table("Category")]
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+    [Table("Product")]
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+    }
+}
